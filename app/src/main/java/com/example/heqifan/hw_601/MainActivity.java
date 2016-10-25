8bcf11c2f1d5ee149505db5badb279b302ffe4b7
QifanHe <heqifan0109@126.com>
2016-10-25 01:04:18 -0400

    This is RSP2.0

--- app/src/main/java/com/example/heqifan/hw_601/MainActivity.java
+++ app/src/main/java/com/example/heqifan/hw_601/MainActivity.java
@@ -5,15 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button buttonRock1;
-    private Button buttonRock2;
-    private Button buttonScissor1;
-    private Button buttonScissor2;
-    private Button buttonPaper1;
-    private Button buttonPaper2;
+    private ImageButton buttonRock;
+    private ImageButton buttonScissor;
+    private ImageButton buttonPaper;
     private Button buttonReset;
     private Button buttonShow;
     private TextView textView;
@@ -25,32 +24,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.textView);
 
-        buttonRock1 = (Button)findViewById(R.id.buttonRock1);
-        buttonRock2 = (Button)findViewById(R.id.buttonRock2);
-        buttonPaper1 = (Button)findViewById(R.id.ButtonPaper1);
-        buttonPaper2 = (Button)findViewById(R.id.ButtonPaper2);
-        buttonScissor1 = (Button)findViewById(R.id.buttonScissor1);
-        buttonScissor2 = (Button)findViewById(R.id.buttonScissor2);
+        buttonRock = (ImageButton) findViewById(R.id.buttonRock);
+        buttonPaper = (ImageButton) findViewById(R.id.buttonPaper);
+        buttonScissor = (ImageButton) findViewById(R.id.buttonScissor);
         buttonReset = (Button)findViewById(R.id.buttonReset);
         buttonShow = (Button)findViewById(R.id.buttonShow);
 
 
-        ButtonListenerRock1 buttonListenerRock1 = new ButtonListenerRock1();
-        ButtonListenerRock2 buttonListenerRock2 = new ButtonListenerRock2();
-        ButtonListenerScissor1 buttonListenerScissor1 = new ButtonListenerScissor1();
-        ButtonListenerScissor2 buttonListenerScissor2 = new ButtonListenerScissor2();
-        ButtonListenerPaper1 buttonListenerPaper1 = new ButtonListenerPaper1();
-        ButtonListenerPaper2 buttonListenerPaper2 = new ButtonListenerPaper2();
+        ButtonListenerRock buttonListenerRock = new ButtonListenerRock();
+        ButtonListenerScissor buttonListenerScissor = new ButtonListenerScissor();
+        ButtonListenerPaper buttonListenerPaper = new ButtonListenerPaper();
         ButtonListenerReset buttonListenerReset = new ButtonListenerReset();
         ButtonListenerShow buttonListenerShow = new ButtonListenerShow();
 
 
-        buttonRock1.setOnClickListener(buttonListenerRock1);
-        buttonRock2.setOnClickListener(buttonListenerRock2);
-        buttonPaper1.setOnClickListener(buttonListenerPaper1);
-        buttonPaper2.setOnClickListener(buttonListenerPaper2);
-        buttonScissor1.setOnClickListener(buttonListenerScissor1);
-        buttonScissor2.setOnClickListener(buttonListenerScissor2);
+        buttonRock.setOnClickListener(buttonListenerRock);
+        buttonPaper.setOnClickListener(buttonListenerPaper);
+        buttonScissor.setOnClickListener(buttonListenerScissor);
         buttonReset.setOnClickListener(buttonListenerReset);
         buttonShow.setOnClickListener(buttonListenerShow);
 
@@ -62,77 +52,76 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class ButtonListenerRock1 implements View.OnClickListener{
+    class ButtonListenerRock implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-            p1 = 1;
-            textView.setText("P2 select");
+            if (p1 == 0){
+                p1 = 1;
+                textView.setText("P2 Select");
+            }
+            else if(p2 == 0){
+                p2 = 1;
+                textView.setText("Click SHOW Button!");
+            }
         }
     }
-    class ButtonListenerRock2 implements View.OnClickListener{
+    class ButtonListenerScissor implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-            p2 = 1;
-            textView.setText("Click show");
+            if (p1 == 0){
+                p1 = 2;
+                textView.setText("P2 Select");
+            }
+            else if(p2 == 0){
+                p2 = 2;
+                textView.setText("Click SHOW Button!");
+            }
         }
     }
-    class ButtonListenerScissor1 implements View.OnClickListener{
+    class ButtonListenerPaper implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-            p1 = 2;
-            textView.setText("P2 select");
-        }
-    }
-    class ButtonListenerScissor2 implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-            p2 = 2;
-            textView.setText("Click show");
-        }
-    }
-    class ButtonListenerPaper1 implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-            p1 = 3;
-            textView.setText("P2 select");
-        }
-    }
-    class ButtonListenerPaper2 implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-            p2 = 3;
-            textView.setText("Click show");
+            if (p1 == 0){
+                p1 = 3;
+                textView.setText("P2 Select");
+            }
+            else if(p2 == 0){
+                p2 = 3;
+                textView.setText("Click SHOW Button!");
+            }
         }
     }
     class ButtonListenerReset implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-            textView.setText("P1 select");
+            p1 = 0;
+            p2 = 0;
+            textView.setText("P1 Select");
         }
     }
     class ButtonListenerShow implements View.OnClickListener{
         @Override
         public void onClick(View v) {
             if (p1 == p2){
-                textView.setText("Tie");
+                textView.setText("Tie!");
             }
             if (p1 == 1 && p2 == 2){
-                textView.setText("P1 WINS");
+                textView.setText("P1 WINS!");
             }
             if (p1 == 2 && p2 == 3){
-                textView.setText("P1 WINS");
+                textView.setText("P1 WINS!");
             }
             if (p1 == 3 && p2 == 1){
-                textView.setText("P1 WINS");
+                textView.setText("P1 WINS!");
             }
             if (p1 == 1 && p2 == 3){
-                textView.setText("P2 WINS");
+                textView.setText("P2 WINS!");
             }
             if (p1 == 2 && p2 == 1){
-                textView.setText("P2 WINS");
+                textView.setText("P2 WINS!");
             }
             if (p1 == 3 && p2 == 2){
-                textView.setText("P2 WINS");
+                textView.setText("P2 WINS!");
             }
         }
     }
